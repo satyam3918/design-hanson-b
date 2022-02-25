e4831af334c0e25e4556436529eab05e92991626
satyam3918 <46595745+satyam3918@users.noreply.github.com>
2022-02-25 12:21:28 +0530

    Add files via upload

---
+++ ChickenBurger.java
@@ -0,0 +1,15 @@
+
+public class ChickenBurger extends Burger {
+
+	@Override
+	public String name() {
+		return "Chicken Burger";
+	}
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 35.0f;
+	}
+
+}
